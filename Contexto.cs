d712ce3e8a9abbee50e9e401185d743341470662
Ernakh <fabriciotonettolondero@gmail.com>
2021-12-20 22:26:39 -0300

    first commit

---
+++ Contexto.cs
@@ -0,0 +1,31 @@
+﻿using AspNetMVCeEF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetMVCeEF
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Email> Emails { get; set; }
+
+        public Contexto()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost;initial Catalog=projetoMVC;User ID=usuario;password=senha;language=Portuguese;Trusted_Connection=True;");
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Email>()
+                .HasOne(e => e.pessoa)
+                .WithMany(e => e.Emails)
+                .OnDelete(DeleteBehavior.ClientCascade);
+        }
+    }
+}
